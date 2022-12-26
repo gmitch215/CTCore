ce7147deb828d60703843c4d83c79b64bf754f57
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-12-25 18:12:32 -0600

    Initial Files

---
+++ src/main/java/me/gamercoder215/mcsurvivors/MCSCore.java
@@ -0,0 +1,14 @@
+package me.gamercoder215.mcsurvivors;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCSCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("MCSCore - Created by GamerCoder");
+
+        getLogger().info("Done!");
+    }
+
+}
